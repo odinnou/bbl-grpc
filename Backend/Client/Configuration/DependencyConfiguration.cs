c1eb72d24387955fba28fa9e758419c07ab0d9aa
rdespelchin <rdespelchin@phoceis.com>
2020-07-13 08:32:04 +0200

    feat(client): add configuration

---
+++ Backend/Client/Configuration/DependencyConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using GrpcConsumer.API;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.InteropServices;
+using static Market.API.AdminProducer;
+
+namespace Client.Configuration
+{
+    public static class DependencyConfiguration
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            #region Repositories
+            services.AddTransient<IMarketApiConsumer, MarketApiConsumer>();
+            #endregion
+
+            return services;
+        }
+    }
+}
---
+++ Backend/Client/Configuration/ThirdPartyConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Client.Configuration
+{
+    public static class ThirdPartyConfiguration
+    {
+        public static IServiceCollection AddThirdParties(this IServiceCollection services)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                AppContext.SetSwitch("System.Net.Http.UseSocketsHttpHandlers", false);
+            }
+
+            #region gRPC
+            //services.AddGrpcClient < Server>(o =>
+            //{
+            //    o.Address = new Uri("");
+            //});
+            #endregion
+
+            return services;
+        }
+    }
+}
